58be55c5c542a0a255baf3c69e880450c7bf68ab
Mason Horder <masonhorder@gmail.com>
2021-12-27 21:21:10 +0000

    now create files to store the show data

--- server.py
+++ server.py
@@ -4,12 +4,23 @@ import os
 
 app = Flask(__name__)
 
+songDirectory = "/home/pi/server/christmaslights/songs"
 
-@app.route('/addSongData', methods=["POST"])
+@app.route('/addSongData', methods=["GET"])
+def addSongData():
+  fileName= request.args.get('file')
+  data = request.args.get('r') + "," + request.args.get('g') + "," + request.args.get('b') + "," + request.args.get('delay')
+  with open(f'{songDirectory}/{fileName}.csv','a') as fd:
+    fd.write(data)
+  return redirect(f"/createSong?file={fileName}")
 
-@app.route('/createSong')
+
+
+@app.route('/createSong', methods=["GET"])
 def createSong():
-  return render_template('create.html')
+  fileName = request.args.get('file')
+  
+  return render_template('create.html', fileName=fileName)
 
 
 @app.route('/home')
@@ -17,10 +28,10 @@ def createSong():
 def home():
   songs = []
   print("finding songs")
-  for file in os.listdir("/home/pi/server/christmaslights/songs"):
+  for file in os.listdir(songDirectory):
     if file.endswith(".csv"):
-      print(os.path.join("/home/pi/server/christmaslights/songs", file))
-      songs.append(os.path.join("/home/pi/server/christmaslights/songs", file))
+      print(os.path.join(songDirectory, file))
+      songs.append(os.path.join(songDirectory, file))
   return render_template('home.html', songs=songs)
 
 
