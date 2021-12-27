61bc101bba29ca5a42bbc7496ab334e29eac0d6b
Mason Horder <masonhorder@gmail.com>
2021-12-27 20:59:34 +0000

    start of server developement

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 from flask import Flask,request,render_template,jsonify,redirect
 import requests
+import os
 
 app = Flask(__name__)
+
+
+@app.route('/addSongData', methods=["POST"])
+
+@app.route('/createSong')
+def createSong():
+  return render_template('create.html')
+
 
 @app.route('/home')
 @app.route('/')
 def home():
-  return render_template('home.html')
+  songs = []
+  print("finding songs")
+  for file in os.listdir("/home/pi/server/christmaslights/songs"):
+    if file.endswith(".csv"):
+      print(os.path.join("/home/pi/server/christmaslights/songs", file))
+      songs.append(os.path.join("/home/pi/server/christmaslights/songs", file))
+  return render_template('home.html', songs=songs)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
